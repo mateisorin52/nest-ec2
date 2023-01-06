c4e45d6a298b887df3f878a0884e49123969e178
Sorin Matei <mateisorin52@yahoo.com>
2023-01-06 17:53:42 +0200

    added dto standard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { User } from '@prisma/client';
+import { UserDto } from 'src/user/dto/userDto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('/login')
-  async loginUser(
-    @Body() user: { email: string; password: string },
-  ): Promise<User> {
+  async loginUser(@Body() user: UserDto): Promise<User> {
     return this.authService.loginUser(user);
   }
 }
